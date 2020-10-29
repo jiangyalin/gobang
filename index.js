748ab948b18dbeb18fbcb5c2b4446611243bad22
yalin.jiang@asapone.cn <995991>
2020-10-29 16:19:24 +0800

    xx

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const latitude = 8
 let situation = []
 
 const init = x => {
+  situation = []
   for (let i = 0; i < x; i++) {
     const array = []
     for (let j = 0; j < x; j++) {
@@ -15,8 +16,10 @@ const init = x => {
   }
 }
 
-const checkWin = matrix => {
+// 检查结束
+const checkWin = (matrix, condition) => {
   const x = matrix.length
+  let _matrix = []
   let BQuantity = 0
   matrix.forEach(item => BQuantity += item.filter(node => node === 'B').length)
   let WQuantity = 0
@@ -27,50 +30,119 @@ const checkWin = matrix => {
   for (let i = 0; i < x; i++) {
     for (let j = 0; j < x; j++) {
       // 检测横向
-      if (blackMatrix[i][j] === holdOn && j < x - 5) {
+      if (blackMatrix[i][j] === holdOn && j < x - condition && !isWin) {
         let win = true
-        for (let k = 0; k < 5; k++) {
+        for (let k = 0; k < condition; k++) {
           if (blackMatrix[i][j + k] !== holdOn) win = false
         }
-        if (win) isWin = true
+        if (win) {
+          isWin = true
+        }
       }
       // 检测竖向
-      if (blackMatrix[i][j] === holdOn && i < x - 5) {
+      if (blackMatrix[i][j] === holdOn && i < x - condition && !isWin) {
         let win = true
-        for (let k = 0; k < 5; k++) {
+        for (let k = 0; k < condition; k++) {
           if (blackMatrix[i + k][j] !== holdOn) win = false
         }
-        if (win) isWin = true
+        if (win) {
+          isWin = true
+        }
       }
       // 检测斜向（右）
-      if (blackMatrix[i][j] === holdOn && i < x - 5 && j < x - 5) {
+      if (blackMatrix[i][j] === holdOn && i < x - condition && j < x - condition && !isWin) {
         let win = true
-        for (let k = 0; k < 5; k++) {
+        for (let k = 0; k < condition; k++) {
           if (blackMatrix[i + k][j + k] !== holdOn) win = false
         }
-        if (win) isWin = true
+        if (win) {
+          isWin = true
+        }
       }
       // 检测斜向（左）
-      if (blackMatrix[i][j] === holdOn && i < x - 5 && j >= 5 - 1) {
+      if (blackMatrix[i][j] === holdOn && i < x - condition && j >= condition - 1 && !isWin) {
         let win = true
-        for (let k = 0; k < 5; k++) {
+        for (let k = 0; k < condition; k++) {
           if (blackMatrix[i + k][j - k] !== holdOn) win = false
         }
-        if (win) isWin = true
+        if (win) {
+          isWin = true
+        }
       }
     }
   }
-  return isWin
+  if (!isWin) _matrix = []
+  return {
+    state: isWin, // 状态
+    identity: holdOn,
+    matrix: _matrix
+  }
 }
 
-const laoZi = (color, matrix) => {
+// 完全随机
+const randomLaoZi = (color, matrix) => {
+  const x = matrix.length
+  let coordinate = ['', '']
   let NQuantity = 0
   matrix.forEach(item => NQuantity += item.filter(node => node === ' ').length)
-  console.log('NQuantity', NQuantity)
-  console.log('randomNum', randomNum(1, NQuantity))
-  // 两步最终
-  // if () {}
-  return []
+  let max = randomNum(1, NQuantity)
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      if (matrix[i][j] === ' ') max--
+      if (max === 0) coordinate = [i, j]
+    }
+  }
+  return {
+    x: coordinate[0],
+    y: coordinate[1]
+  }
+}
+
+// 最优
+const optimalLaoZi = (color, matrix) => {
+  const arr = []
+  const max = matrix.length
+  for (let i = 0; i < max; i++) {
+    for (let j = 0; j < max; j++) {
+      if (matrix[i][j] === ' ') {
+        arr.push({
+          probability: winRate(color, matrix),
+          x: i,
+          y: j
+        })
+      }
+    }
+  }
+  let x = ''
+  let y = ''
+  let probability = 0
+  arr.forEach(item => {
+    if (item.probability > probability) {
+      probability = item.probability
+      x = item.x
+      y = item.y
+    }
+  })
+  return {
+    x,
+    y
+  }
+}
+
+// 连4
+const laoZiB = (color, matrix) => {
+  const x = matrix.length
+  let coordinate = [0, 0]
+  let NQuantity = 0
+  matrix.forEach(item => NQuantity += item.filter(node => node === ' ').length)
+  let max = randomNum(1, NQuantity)
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      if (matrix[i][j] === ' ') max--
+      if (max === 0) coordinate = [i, j]
+    }
+  }
+  return coordinate
 }
 
 const randomNum = (minNum, maxNum, fixed = 0) => {
@@ -79,34 +151,84 @@ const randomNum = (minNum, maxNum, fixed = 0) => {
   return (minNum + differ * random).toFixed(fixed)
 }
 
+const winRate = (color = 'B', matrix) => {
+  const max = 100
+  let stepCount = 0
+  for (let k = 0; k < max; k++) {
+    const _matrix = matrix.map(item => item.map(node => node))
+    let piece = color
+    for (let i = 0; i < warp; i++) {
+      for (let j = 0; j < warp; j++) {
+        const check = checkWin(_matrix, 5)
+        if (!check.state) {
+          const {x, y} = randomLaoZi(piece, _matrix)
+          _matrix[x][y] = piece
+          piece = piece === 'B' ? 'W' : 'B'
+        } else if (check.identity === color) {
+          i = warp
+          j = warp
+          stepCount++
+        } else {
+          i = warp
+          j = warp
+        }
+      }
+    }
+  }
+  return (stepCount / max).toFixed(9)
+}
+
 init(warp)
 
 // situation[0][0] = 'B'
 // situation[0][1] = 'B'
 // situation[0][2] = 'B'
-// situation[0][3] = 'B'
-// situation[0][4] = 'B'
+//
+// situation[1][0] = 'W'
+// situation[1][2] = 'W'
+// situation[2][0] = 'W'
 
-// situation[0][0] = 'B'
-// situation[1][0] = 'B'
-// situation[2][0] = 'B'
-// situation[3][0] = 'B'
-// situation[4][0] = 'B'
+const auto = (matrix, showLog = true) => {
+  const check = checkWin(matrix, 5)
+  if (!check.state) {
+    if (piece === 'B') {
+      const aLaoZi = optimalLaoZi(piece, matrix)
+      if (showLog) console.log('AwinRate', aLaoZi)
+      if (aLaoZi.x === '') return null
+      matrix[aLaoZi.x][aLaoZi.y] = piece
+    } else {
+      const aLaoZi = randomLaoZi(piece, matrix)
+      if (showLog) console.log('BwinRate', aLaoZi)
+      if (aLaoZi.x === '') return null
+      matrix[aLaoZi.x][aLaoZi.y] = piece
+    }
+    if (showLog) console.log(matrix)
+    piece = piece === 'B' ? 'W' : 'B'
+    return auto(matrix, showLog)
+  } else {
+    console.log(matrix)
+    return check
+  }
+}
 
-// situation[0][0] = 'B'
-// situation[1][1] = 'B'
-// situation[2][2] = 'B'
-// situation[3][3] = 'B'
-// situation[4][4] = 'B'
+const confrontation = (bo, matrix) => {
+  const history = []
+  if (bo > 0) {
+    const recording = auto(matrix, false)
+    history.push(recording)
+    console.log('recording', recording)
+    bo--
+    return confrontation(bo, matrix)
+  } else {
+    return history
+  }
+}
 
-situation[0][4] = 'B'
-situation[1][3] = 'B'
-situation[2][2] = 'B'
-situation[3][1] = 'B'
-// situation[4][0] = 'B'
+let piece = 'B'
 
-console.log(situation)
+// console.log('confrontation', confrontation(10, situation))
+console.log('auto', auto(situation))
 
-console.log('laoZi', laoZi('B', situation))
-
-console.log('checkWin', checkWin(situation))
+// B  1111111
+// W
+// NULL  111
